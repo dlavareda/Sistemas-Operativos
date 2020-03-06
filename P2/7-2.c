1d23c1bf8c53ebf739a80ac3b2737b91bd4b956a
dlavareda <diogolavareda9@hotmail.com>
2020-03-06 10:29:30 +0000

    update

---
+++ P2/7-2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUFFSIZE 128
+void ioCopy(int IN, int OUT);
+/*int main()
+{
+    ioCopy(STDIN_FILENO, STDOUT_FILENO); // 0 , 1
+    return (0);
+}*/
+int main(int argc, char *argv[])
+{
+    int fdIn = open(argv[1], O_RDONLY);
+    if (argc == 1)
+    {
+        printf("Nenhum argumento!\n");
+        return 0;
+    }
+
+    if (fdIn != 0)
+    {
+        int fdOut = creat(argv[2], S_IRUSR | S_IWUSR);
+        ioCopy(fdIn, fdOut);
+    }
+    else
+    {
+        perror("Ficheiro original vazio!\n");
+    }
+    return 0;
+}
+void ioCopy(int IN, int OUT) //no error reporting
+{
+    int n;
+    char buf[BUFFSIZE];
+    while ((n = read(IN, buf, BUFFSIZE)) > 0)
+    {
+        if (write(OUT, buf, n) != n)
+            perror("Erro de escrita!\n");
+    }
+    if (n < 0)
+        perror("Erro de leitura!\n");
+}
---
+++ P2/7.c
@@ -0,0 +1,25 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+//for ints
+/*int main()
+{
+ int fd, i, vec[10];
+ for (i=0;i <10; i++)
+ vec[i]=i+512;
+ fd = creat("/home/dlavareda/Documents/UBI/2019-2020/SIstemas Operativos/P2/test.bin", S_IRUSR| S_IWUSR ) ;
+ write(fd, vec , sizeof(int)*10);
+ return (0);
+}*/
+
+//for shorts
+int main()
+{
+ int fd, i, vec[10];
+ for (i=0;i <10; i++)
+ vec[i]=i+512;
+ fd = creat("/home/dlavareda/Documents/UBI/2019-2020/SIstemas Operativos/P2/test.bin", S_IRUSR| S_IWUSR ) ;
+ write(fd, vec , sizeof(short)*10);
+ return (0);
+}
