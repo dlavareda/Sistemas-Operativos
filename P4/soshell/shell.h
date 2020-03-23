61cf8492146f2639ce8b8dfa215b3dfce1cd0074
dlavareda <diogolavareda9@hotmail.com>
2020-03-23 22:34:37 +0000

    update

---
+++ P4/soshell/shell.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <sys/wait.h>
+
+void parse(char *buf, char **args);
+
+void execute(char **args);
+
+int builtin(char **args);
+
+void socp(char *, char *);
+
+void ioCopy(int, int);
+/* constantes que podem tornar uteis*/
+
+#define BG 0
+#define FG 1
---
+++ P4/soshell/socp.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+void socp(char *fonte, char *destino)
+{
+    int fdIn = open(fonte, O_RDONLY);
+    if (fdIn > 0)
+    {
+        int fdOut = creat(destino, S_IRUSR | S_IWUSR);
+        ioCopy(fdIn, fdOut);
+    }
+    return 0;
+}
+void ioCopy(int IN, int OUT)
+{
+    int n;
+    char *buf = malloc(16);
+    while ((n = read(IN, buf, 16)) > 0)
+    {
+        if (write(OUT, buf, n) != n)
+            perror("Erro de escrita!\n");
+    }
+    if (n < 0)
+        perror("Erro de leitura!\n");
+}
